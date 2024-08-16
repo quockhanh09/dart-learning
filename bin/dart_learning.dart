697b7e9c0edd515a2cba11d19a82e41f399d2e15
quockhanh09 <dzsatthu8@gmail.com>
2024-08-16 20:46:39 +0700

    dart_learning

--- bin/dart_learning.dart
+++ bin/dart_learning.dart
@@ -1,5 +1,5 @@
-import 'package:dart_learning/dart_learning.dart' as dart_learning;
+import 'package:dart_learning/dart_lib.dart' ;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_learning.calculate()}!');
+  print('Hello world: ${calculate()}!');
 }
--- test/dart_learning_test.dart
+++ test/dart_learning_test.dart
@@ -1,8 +1,15 @@
-import 'package:dart_learning/dart_learning.dart';
+import 'package:dart_learning/dart_lib.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('calculate', () {
+    expect(calculate(), 42);
+  });
+  test('calculate', () {
+    expect(calculate(), 42);
+  });test('calculate', () {
+    expect(calculate(), 42);
+  });test('calculate', () {
     expect(calculate(), 42);
   });
 }
